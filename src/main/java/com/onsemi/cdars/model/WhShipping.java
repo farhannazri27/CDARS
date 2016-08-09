b0a6b7ac4f1f1bb59e2c4f16dd4d2d5a4e431585
farhannazri27 <cdarsrel@gmail.com>
2016-08-09 18:39:37 +0800

    test

---
+++ src/main/java/com/onsemi/cdars/model/WhShipping.java
@@ -0,0 +1,227 @@
+package com.onsemi.cdars.model;
+
+public class WhShipping {
+
+    private String id;
+    private String requestId;
+    private String boxId;
+    private String materialPass;
+    private String hardwareBarcode1;
+    private String dateScan1;
+    private String hardwareBarcode2;
+    private String dateScan2;
+    private String shippingDate;
+    private String status;
+    private String remarks;
+    private String flag;
+    private String createdBy;
+    private String createdDate;
+    private String modifiedBy;
+    private String modifiedDate;
+    private String mpNo;
+    private String mpExpiryDate;
+
+    //to be merge with whRequest
+    private String requestEquipmentType;
+    private String requestEquipmentId;
+    private String requestQuantity;
+    private String requestType;
+    private String requestRequestedBy;
+    private String requestRequestedDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getBoxId() {
+        return boxId;
+    }
+
+    public void setBoxId(String boxId) {
+        this.boxId = boxId;
+    }
+
+    public String getMaterialPass() {
+        return materialPass;
+    }
+
+    public void setMaterialPass(String materialPass) {
+        this.materialPass = materialPass;
+    }
+
+    public String getHardwareBarcode1() {
+        return hardwareBarcode1;
+    }
+
+    public void setHardwareBarcode1(String hardwareBarcode1) {
+        this.hardwareBarcode1 = hardwareBarcode1;
+    }
+
+    public String getDateScan1() {
+        return dateScan1;
+    }
+
+    public void setDateScan1(String dateScan1) {
+        this.dateScan1 = dateScan1;
+    }
+
+    public String getHardwareBarcode2() {
+        return hardwareBarcode2;
+    }
+
+    public void setHardwareBarcode2(String hardwareBarcode2) {
+        this.hardwareBarcode2 = hardwareBarcode2;
+    }
+
+    public String getDateScan2() {
+        return dateScan2;
+    }
+
+    public void setDateScan2(String dateScan2) {
+        this.dateScan2 = dateScan2;
+    }
+
+    public String getShippingDate() {
+        return shippingDate;
+    }
+
+    public void setShippingDate(String shippingDate) {
+        this.shippingDate = shippingDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getRequestEquipmentType() {
+        return requestEquipmentType;
+    }
+
+    public void setRequestEquipmentType(String requestEquipmentType) {
+        this.requestEquipmentType = requestEquipmentType;
+    }
+
+    public String getRequestEquipmentId() {
+        return requestEquipmentId;
+    }
+
+    public void setRequestEquipmentId(String requestEquipmentId) {
+        this.requestEquipmentId = requestEquipmentId;
+    }
+
+    public String getRequestQuantity() {
+        return requestQuantity;
+    }
+
+    public void setRequestQuantity(String requestQuantity) {
+        this.requestQuantity = requestQuantity;
+    }
+
+    public String getRequestType() {
+        return requestType;
+    }
+
+    public void setRequestType(String requestType) {
+        this.requestType = requestType;
+    }
+
+    public String getMpNo() {
+        return mpNo;
+    }
+
+    public void setMpNo(String mpNo) {
+        this.mpNo = mpNo;
+    }
+
+    public String getMpExpiryDate() {
+        return mpExpiryDate;
+    }
+
+    public void setMpExpiryDate(String mpExpiryDate) {
+        this.mpExpiryDate = mpExpiryDate;
+    }
+
+    public String getRequestRequestedBy() {
+        return requestRequestedBy;
+    }
+
+    public void setRequestRequestedBy(String requestRequestedBy) {
+        this.requestRequestedBy = requestRequestedBy;
+    }
+
+    public String getRequestRequestedDate() {
+        return requestRequestedDate;
+    }
+
+    public void setRequestRequestedDate(String requestRequestedDate) {
+        this.requestRequestedDate = requestRequestedDate;
+    }
+    
+    
+    
+
+}
